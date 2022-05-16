0914b83e8ccfc1e0c27ca17170dc1b84de31f27e
Termont Didier <didier.termont@skynet.be>
2022-05-16 09:34:14 +0200

    Separate R code for the report only question 1 - 4 5 on its way

---
+++ DeRonge-Termont-Visschers_Rcode.R
@@ -0,0 +1,98 @@
+library(ggplot2)
+library(coda)
+library(rjags)
+library(R2WinBUGS)
+
+# Step O - Data prep 
+rm(list=ls())
+cannabis <- read.csv("cannabis.txt",sep="",header = TRUE)
+
+# Question 3
+## Functions for the Metropolis algorithm
+log.f.2 <- function(pi,N,xplus) {
+  (N-xplus)*log(1-pi)+xplus*log(1+35*pi)
+}
+
+metropolis <- function(pi0,M, sd.prop, lpost, N, xplus) {
+  pi = numeric(M)
+  pi[1]=pi0
+  
+  n.accept = 0
+  for(i in 2:M) {
+    pi.prop = pi[i-1]+rnorm(1,0,sd.prop)
+    prop = min(1,exp(lpost(pi.prop,N,xplus) - lpost(pi[i-1],N,xplus)))
+    accept = (runif(1) <= prop)
+    if(accept) {
+      n.accept = n.accept+1
+      pi[i] = pi.prop
+    } else {
+      pi[i] = pi[i-1]
+    }
+  }
+  
+  accept.rate = round(n.accept/(M-1),2)
+  cat("Acceptance rate: ",accept.rate,"\n")
+  
+  return(pi)
+}
+
+## Running of the metropolis algo on the full population
+M <- 10000
+pi = metropolis(
+  pi0=0.5,M,sd=0.028,
+  lpost=log.f.2,N=nrow(cannabis),xplus=sum(cannabis$y))
+
+## Convergence
+## acceptance rate is 40% : OK (univariate)
+## 
+geweke.diag(mcmc(pi))
+geweke.plot(mcmc(pi),nbins=10)
+
+## summary
+traceplot(mcmc(pi))
+quantile(pi[200:M],c(0.025,0.975))
+HPDinterval(mcmc(pi[200:M]))
+
+
+# Question 4
+## Metropolis for the male subset
+can_m =subset(cannabis,male==1)
+pi_m = metropolis(
+  pi0=0.5,M,sd.prop=0.040,
+  lpost=log.f.2,N=nrow(can_m),xplus=sum(can_m$y))
+
+## Convergence
+geweke.diag(mcmc(pi_m))
+geweke.plot(mcmc(pi_m),nbins=10)
+
+## summary
+traceplot(mcmc(pi_m))
+quantile(pi_m[200:M],c(0.025,0.975))
+HPDinterval(mcmc(pi_m[200:M]))
+
+## Metropolis for the female subset
+can_f=subset(cannabis,male==0)
+pi_f = metropolis(
+  pi0=0.5,M,sd.prop=0.0255,
+  lpost=log.f.2,N=nrow(can_f),xplus=sum(can_f$y))
+
+## Convergence
+geweke.diag(mcmc(pi_f))
+geweke.plot(mcmc(pi_f),nbins=10)
+
+## summary
+traceplot(mcmc(pi_f))
+quantile(pi_f[200:M],c(0.025,0.975))
+HPDinterval(mcmc(pi_f[200:M]))
+
+## What about the delta
+delta = pi_m[200:M] - pi_f[200:M]
+
+## convergence
+geweke.diag(mcmc(delta))
+geweke.plot(mcmc(delta))
+
+## summary
+traceplot(mcmc(delta))
+densplot(mcmc(delta))
+HPDinterval(mcmc(delta))
